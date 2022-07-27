c8af54fccb59c9121934f8d4edfcdcf7e4c6095f
Jonathan Taylor <jonathan.taylor@n-able.com>
2022-07-27 17:07:54 +0100

    TF Pipeline

--- vars/checkForJenkinsMasterUpdates.groovy
+++ vars/checkForJenkinsMasterUpdates.groovy
@@ -1,55 +1,60 @@
 #!/usr/bin/env groovy
 
-def call(planPath){
+def call(planFile, optionalFolderPath){
 
-  sh 'terraform -chdir=./cicd/pipelines/terraform/ show -json ' + planPath + ' > tfPlan.log'
-
-  def resultString = readFile(file: 'tfPlan.log');
-  def results = resultString.split('\n')
-
-  def enhancedWarning = false;
-  def triggeringChange;
-  def output;
-
-  for (def result in results){
-    try{
-      output = readJSON text:result;
-    } catch (Exception ex){
+    if (optionalFolderPath){
+        sh 'terraform -chdir=' + optionalFolderPath + ' show -json ' + planFile + ' > tfPlan.log'
+    } else {
+        sh 'terraform show -json ' + plan + ' > tfPlan.log'
     }
-  }
+    
 
-  if (output){
-    if (output.resource_changes){
-      for (def resource_change in output.resource_changes){
-        if (resource_change.change){
-          if (resource_change.change.actions.indexOf('update') > -1){
+    def resultString = readFile(file: 'tfPlan.log');
+    def results = resultString.split('\n')
 
-            def manifestBeforeStr = resource_change.change.before.manifest;
-            def manifestAfterStr = resource_change.change.after.manifest;
+    def enhancedWarning = false;
+    def triggeringChange;
+    def output;
 
-            def manifestBefore = readJSON text:manifestBeforeStr;
-            def manifestAfter = readJSON text:manifestAfterStr;
-
-            if (manifestBefore.kind == 'Jenkins'){
-              if (manifestBefore.spec && manifestBefore.spec.master){
-                def compBefore = manifestBefore.spec.master;
-                def compAfter = manifestAfter.spec.master;
-
-                if (compBefore != compAfter){
-                  enhancedWarning = true;
-                  triggeringChange = compAfter;
-                  echo "Master node config is changed"
-                } 
-              }
-            } 
-          }
+    for (def result in results){
+        try{
+        output = readJSON text:result;
+        } catch (Exception ex){
         }
-      }
     }
-  } else {
-    echo "Invalid TF Plan JSON"
-  }
 
-  sh 'rm -f tfPlan.log'
-  return enhancedWarning;
+    if (output){
+        if (output.resource_changes){
+        for (def resource_change in output.resource_changes){
+            if (resource_change.change){
+            if (resource_change.change.actions.indexOf('update') > -1){
+
+                def manifestBeforeStr = resource_change.change.before.manifest;
+                def manifestAfterStr = resource_change.change.after.manifest;
+
+                def manifestBefore = readJSON text:manifestBeforeStr;
+                def manifestAfter = readJSON text:manifestAfterStr;
+
+                if (manifestBefore.kind == 'Jenkins'){
+                if (manifestBefore.spec && manifestBefore.spec.master){
+                    def compBefore = manifestBefore.spec.master;
+                    def compAfter = manifestAfter.spec.master;
+
+                    if (compBefore != compAfter){
+                    enhancedWarning = true;
+                    triggeringChange = compAfter;
+                    echo "Master node config is changed"
+                    } 
+                }
+                } 
+            }
+            }
+        }
+        }
+    } else {
+        echo "Invalid TF Plan JSON"
+    }
+
+    sh 'rm -f tfPlan.log'
+    return enhancedWarning;
 }
