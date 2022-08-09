e59286ecf5316c7d3456d2d32e759e1eea19e0f3
Jonathan Taylor <jonathan.taylor@n-able.com>
2022-08-09 12:48:22 +0100

    Add seed job builder

--- vars/northstar.groovy
+++ vars/northstar.groovy
@@ -108,5 +108,75 @@ boolean checkForJenkinsMasterUpdates(planFilePath){
     return enhancedWarning;
 }
 
+Object buildSeedJobs(seedJobs){
+
+// TODO: Function to build seed jobs from input
+//
+// Plugin Dependencies: 
+//        TODO: Plugin dependencies
+//
+// Inputs:
+//        TODO: Input descriptions
+//
+// Returns: 
+//        TODO: Output description
+
+    multibranchPipelineJob('terraform-pipeline') {
+    branchSources {
+            branchSource {
+                source {
+                    github {
+                        id('terraform-pipeline')
+                        repoOwner('JonTaylor-NAble')
+                        repository('jenkins-local-pipelines')
+                        credentialsId('github-account')
+                        buildOriginBranch(true)
+                        buildOriginPRHead(true)
+                        repositoryUrl('')
+                        configuredByUrl(false)
+                    }
+                }
+                strategy {
+                allBranchesSame {
+                        props {
+                            suppressAutomaticTriggering {
+                                strategy('INDEXING')
+                            }
+                        }
+                    }
+                }
+            }
+    }
+
+    orphanedItemStrategy {
+        discardOldItems {
+        daysToKeep(30)
+        numToKeep(30)
+        }
+    }
+
+    factory {
+        workflowBranchProjectFactory {
+        scriptPath('cicd/pipelines/terraform/terraform.groovy')
+        }
+    }
+
+    configure {
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
+        strategyId(1) // Enable support for discovering github branches on this repo
+        }
+        traits << 'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
+        strategyId(2) // Enable support for discovering PullRequests to this github repo
+        }
+        traits << 'jenkins.plugins.git.traits.CleanBeforeCheckoutTrait' {
+        extension(class: 'hudson.plugins.git.extensions.impl.CleanBeforeCheckout') {
+            deleteUntrackedNestedRepositories(true)
+        }
+        }
+    }
+    }
+}
+
 //If this library is not loaded 'implicitly', uncomment the line below:
 //return this
