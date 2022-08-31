2dc18258a870ef6b6f80d905b42e89c842d804ea
Jonathan Taylor <jonathan.taylor@n-able.com>
2022-08-31 12:43:10 +0100

    Logging

--- vars/northstar.groovy
+++ vars/northstar.groovy
@@ -198,6 +198,7 @@ String getSeedJobDSL(yamlPath){
         for (repo in data.repos){
             repoData = populateRepoDefaults(repo);
             buildLists(repoData);
+            echo(repoData);
 
             if (repoData.validity){
                 def dslScript = buildTemplate(repoData);
